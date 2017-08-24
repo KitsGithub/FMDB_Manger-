74673243e94e280774ef7166ecda2bd8415f06f7
kit <mac@daxiong.local>
2017-08-24 15:25:20 +0800

    新建项目 && 导入FMDB

---
+++ FMDB_Manager/FMDB_Manager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDB_Manager
+//
+//  Created by mac on 2017/8/24.
+//  Copyright © 2017年 kit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
